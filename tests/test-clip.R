901e40efbb891da8892956b02c83cd0d96528fa1
Paul Murrell <paul@stat.auckland.ac.nz>
2014-10-29 13:46:06 +1300

    added support for C_layout and C_clip

---
+++ tests/test-clip.R
@@ -0,0 +1,16 @@
+
+library(gridGraphics)
+
+clip1 <- function() {
+    x <- rnorm(1000)
+    hist(x, xlim = c(-4,4))
+    usr <- par("usr")
+    clip(usr[1], -2, usr[3], usr[4])
+    hist(x, col = 'red', add = TRUE)
+    clip(2, usr[2], usr[3], usr[4])
+    hist(x, col = 'blue', add = TRUE)
+}
+
+plotdiff(expression(clip1()), "clip-1")
+
+plotdiffResult()
