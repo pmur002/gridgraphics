f0717c290645e96be49ac23e6e8e0ddba17ee3b1
Paul Murrell <paul@stat.auckland.ac.nz>
2014-10-29 14:45:41 +1300

    added support for C_path

---
+++ R/path.R
@@ -0,0 +1,21 @@
+
+# path(x, y, lengths, rule, col, border, lty, ...)
+
+C_path <- function(x) {
+    dev.set(recordDev())
+    par <- currentPar(x[-(1:8)])
+    dev.set(playDev())
+    depth <- gotovp(par$xpd)
+    xx <- x[[2]]
+    yy <- x[[3]]
+    lengths <- x[[4]]
+    rule <- x[[5]]
+    col <- x[[6]]
+    border <- x[[7]]
+    lty <- x[[8]]
+    grid.path(xx, yy, default="native",
+              id.lengths=lengths, rule=rule,
+              gp=gpar(col=border, fill=col, lty=lty),
+              name=grobname("path"))
+    upViewport(depth)    
+}
