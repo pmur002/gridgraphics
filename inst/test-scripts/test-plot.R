2fe06ae1245db761eb5f3e8f453583433d569b69
Paul Murrell <paul@stat.auckland.ac.nz>
2014-11-10 13:54:45 +1300

    added tests for plot.table()

--- inst/test-scripts/test-plot.R
+++ inst/test-scripts/test-plot.R
@@ -1,12 +1,32 @@
 
 library(gridGraphics)
 
-plotdiff(expression(plot(1)), "plot-1")
-plotdiff(expression(plot(1:10)), "plot-10")
-plotdiff(expression(plot(1:10, main="title")), "plot-title")
-plotdiff(expression(plot(1:10, sub="byline")), "plot-sub")
-plotdiff(expression(pie(1:10)), "pie")
-# Test margin labelling when multiple plots on page (so cexbase=.66)
-plotdiff(expression({ par(mfrow=c(2, 2)); plot(1:10) }), "plot-mfrow")
+require(stats)
+
+plot1 <- function() {
+    plot(cars)
+    lines(lowess(cars))
+}
+
+plot2 <- function() {
+    plot(sin, -pi, 2*pi) # see ?plot.function
+}
+
+plot3 <- function() {
+    ## Discrete Distribution Plot:
+    plot(table(rpois(100, 5)), type = "h", col = "red", lwd = 10,
+         main = "rpois(100, lambda = 5)")
+}
+
+plot4 <- function() {
+    ## Simple quantiles/ECDF, see ecdf() {library(stats)} for a better one:
+    plot(x <- sort(rnorm(47)), type = "s", main = "plot(x, type = \"s\")")
+    points(x, cex = .5, col = "dark red")
+}
+
+plotdiff(expression(plot1()), "plot-1")
+plotdiff(expression(plot2()), "plot-2")
+plotdiff(expression(plot3()), "plot-3")
+plotdiff(expression(plot4()), "plot-4")
 
 plotdiffResult()
