b5b6920a72a283dbebe3945d7a82afe30c887078
jason <374567155@qq.com>
2017-02-27 16:23:45 +1300

    updated

--- R/filled.r
+++ R/filled.r
@@ -3,19 +3,17 @@ FindPolygonVertices = function(low,  high,
 		     z11,  z21,  z12,  z22,
 		     x,  y,  z, npt)
 {
+    npt = 1
     out = FindCutPoints(low, high, x1,  y1,  z11, x2,  y1,  z21, x, y, z, npt)
-    low <<- low; high <<- high; y1 <<- y1; x2 <<- x2; z21 <<- z21; x2 <<- x2; z22 <<- z22
-    d <<- out
     x = out$x; y = out$y; z = out$z; npt = out$npt
-    out = FindCutPoints(low, high, y1,  x2,  z21, y2,  x2,  z22, y, x, z, npt)
-    dd <<- out
     
+    out = FindCutPoints(low, high, y1,  x2,  z21, y2,  x2,  z22, y, x, z, npt)
     x = out$x; y = out$y; z = out$z; npt = out$npt
+    
     out = FindCutPoints(low, high, x2,  y2,  z22, x1,  y2,  z12, x, y, z, npt)
-    ddd<<-out
     x = out$x; y = out$y; z = out$z; npt = out$npt
+    
     out = FindCutPoints(low, high, y2,  x1,  z12, y1,  x1,  z11, y, x, z, npt)
-    dddd <<- out
     out
     
 }
@@ -25,56 +23,52 @@ FindPolygonVertices = function(low,  high,
 
 C_filledcontour = function(plot)
 {
-    plot.info = plot[[1]][[11]][[2]]
+    plot.info = plot[[1]][[12]][[2]]
     x <<- plot.info[[2]]
     y <<- plot.info[[3]]
     z <<- plot.info[[4]]
     sc <<- plot.info[[5]]
     px = py = pz = numeric(8)
     ## not sure if we need the "FixupCol" or not 
-    scol = FixupCol(plot.info[[6]])
+    scol <<- FixupCol(plot.info[[6]])
 
-    nx = length(x)
-    ny = length(y)
+    nx <<- length(x)
+    ny <<- length(y)
     if (nx < 2 || ny < 2) stop("insufficient 'x' or 'y' values")
 
     ## do it this way as coerceVector can lose dims, e.g. for a list matrix
     if (nrow(z) != nx || ncol(z) != ny) stop("dimension mismatch")
  
-    nc = length(sc)
+    nc <<- length(sc)
     if (nc < 1) warning("no contour values")
 
     ncol = length(scol)
     npt = 0
-    #/* Shorthand Pointers */
-    
-    vp = plotViewport(par('usr'), xscale = c(1,20), yscale = c(1, 20),name = 'clipoff',
-                    clip = 'off')
+    grid.newpage()
+    vp = plotViewport(par('usr'), xscale = range(x, na.rm = TRUE), 
+                        yscale = range(y, na.rm = TRUE),
+                        name = 'clipoff', clip = 'off')
     pushViewport(vp)
+    upViewport()
     
     count = 0
-    nx = ny = nc = 2
-    nc = 2
-    for(i in 1:nx){
-    for(j in 1:ny){
-        for(k in 1:nc){
-            print(paste("k = ", k))
-            if(count == 0)  npt = 1; count = count + 1
-            out <<- FindPolygonVertices(low = sc[k], high = sc[k + 1],
+    for(i in 1:(nx - 1)){
+    for(j in 1:(ny - 1)){
+        for(k in 1:(nc - 1)){
+            if(count == 0)  npt = 1; count = count + 1            
+            out = FindPolygonVertices(low = sc[k], high = sc[k + 1],
                     x1 = x[i], x2 = x[i + 1],
                     y1 = y[j], y2 = y[j + 1],
-                    z11 = z[i + (j) * nx],
-                    z21 = z[(i + 1) + (j) * nx],
-                    z12 = z[i + (j + 1) * nx],
-                    z22 = z[(i + 1) + (j + 1) * nx],
+                    z11 = z[i, (j) ],
+                    z21 = z[(i + 1), (j) ],
+                    z12 = z[i, (j + 1) ],
+                    z22 = z[(i + 1), (j + 1) ],
                     x = px, y = py, z = pz, npt = npt)
+            
             npt = out$npt
-            #x = out$x
-            #y = out$y
-            #z = out$z
             if(npt > 2)
             {
-                grid.polygon(out$x, out$y,default.units = 'native')
+                grid.polygon(out$x, out$y,default.units = 'native', vp = 'clipoff')
             }
         }
     }
--- R/if.r
+++ R/if.r
@@ -1,44 +1,34 @@
 FindCutPoints = function(low, high, x1, y1, z1, x2, y2, z2,
-                        x, y, z, npt){
-    print(z2)
-    
-    if( z1 > z2)
+                        x, y, z, npt){    
+    if( z1 > z2){
+    if( z2 > high || z1 < low) {
+        return(out = list(x = x, y = y, z = z, npt = npt))}
+    if(z1 < high){   
+        x[npt] = x1
+        y[npt] = y1
+        z[npt] = z1
+        npt = npt + 1
+    } else if(z1 == Inf)
     {
-        if( z2 > high || z1 < low) {
-        #return(out = list(x = x, y = y, z = z, npt = npt))
-        }
-        if(z1 < high)
-        {   
-            print("if1")
-            x[npt] = x1
-            y[npt] = y1
-            z[npt] = z1
-            npt = npt + 1
-        } else if(z1 == Inf)
-        {
-            print("if2")
-            x[npt] = x2
-            y[npt] = y1
-            z[npt] = z2
-            npt = npt + 1
-        } else{
-            print("if3")
-            c = (z1 - high)/(z1 - z2)
-            x[npt] = x1 + c * (x2 - x1)
-            y[npt] = y1
-            z[npt] = z1 + c* (z2 - z1)
-            npt = npt + 1
-        }
+        x[npt] = x2
+        y[npt] = y1
+        z[npt] = z2
+        npt = npt + 1
+    } else{
+        c = (z1 - high)/(z1 - z2)
+        x[npt] = x1 + c * (x2 - x1)
+        y[npt] = y1
+        z[npt] = z1 + c * (z2 - z1)
+        npt = npt + 1
+    }
     ##
     if (z2 == -Inf) {
-    print("if4")
 	    x[npt] = x1
 	    y[npt] = y1
 	    z[npt] = z1
 	    npt = npt + 1
 	} else if (z2 <= low) { 
-    print("if5")
-	    c = (z2 -low) / (z2 - z1)
+	    c = (z2 - low) / (z2 - z1)
 	    x[npt] = x2 - c * (x2 - x1)
 	    y[npt] = y1
 	    z[npt] = z2 - c * (z2 - z1)
@@ -46,11 +36,8 @@ FindCutPoints = function(low, high, x1, y1, z1, x2, y2, z2,
 	}
     
     } else if (z1 < z2) {
-    print("if6")
 	if (z2 < low || z1 > high){
-    print("if7")
-        #return(out = list(x = x, y = y, z = z, npt = npt))
-        }
+        return(out = list(x = x, y = y, z = z, npt = npt))}
 	if (z1 > low) {
 	    x[npt] = x1
 	    y[npt] = y1
