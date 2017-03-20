aaf96f1198f7e69961f2ff76b2be04dc21bbfbd7
jason <374567155@qq.com>
2017-03-20 21:58:30 +1300

    done

--- inst/test-scripts/test-filled.contour.R
+++ inst/test-scripts/test-filled.contour.R
@@ -1,3 +1,5 @@
+library(gridGraphics)
+
 f1 = function(){
     x = 10*1:nrow(volcano)
     y = 10*1:ncol(volcano)
@@ -6,16 +8,16 @@ f1 = function(){
     filled.contour(x = 1:20, y = 1:20, z = b)
 }
 
-plotdiff(expression(f1()), "f1")
+
 
 f2 = function(){
     x = y = seq(-4*pi, 4*pi, len = 30)
     r = sqrt(outer(x^2, y^2, "+"))
     filled.contour(cos(r^2)*exp(-r/(2*pi)), axes = FALSE)
-    ## rather, the key *should* be labeled:
-    filled.contour(cos(r^2)*exp(-r/(2*pi)), frame.plot = FALSE,
-                   plot.axes = {})
-
 }
 
-plotdiff(expression(f2()), "f2")
+plotdiff(expression(f1()), "f11")
+plotdiff(expression(f2()), "f21")
+
+
+plotdiffResult()
--- inst/test-scripts/test-persp.R
+++ inst/test-scripts/test-persp.R
@@ -72,89 +72,69 @@ testPersp2 = function(theta=120, phi = 20, expand = 0.5, col = 'orange ',
 }
 
 
-plotdiff(expression(testPersp()), 'sin')
-plotdiff(expression(testPersp1()), 'sin2')
-plotdiff(expression(testPersp2()), 'Torus')
+plotdiff(expression(testPersp(box = FALSE)), 'sin')
+plotdiff(expression(testPersp1(box = FALSE)), 'sin2')
+plotdiff(expression(testPersp2(box = FALSE)), 'Torus')
 
 ## test on theta
-	 
-	plotdiff(expression(testPersp(30)), 'persp-2')
-	plotdiff(expression(testPersp(175)), 'persp-3')
-	plotdiff(expression(testPersp(285)), 'persp-4')
+	plotdiff(expression(testPersp(30)), 'persp-1')
 
 ## test on phi
-	plotdiff(expression(testPersp(phi = 15)), 'persp-5')
-	plotdiff(expression(testPersp(phi = 22.5)), 'persp-6')
-	plotdiff(expression(testPersp(phi = 80)), 'persp-7')
-	plotdiff(expression(testPersp(phi = 310)), 'persp-8')
+	plotdiff(expression(testPersp(phi = 15)), 'persp-2')
+
 ## test on expand
-	plotdiff(expression(testPersp(epand = 0.01)), 'persp-9')
-	plotdiff(expression(testPersp(epand = 0.2)), 'persp-10')
-	plotdiff(expression(testPersp(epand = 2)), 'persp-11')
-	plotdiff(expression(testPersp(epand = 10)), 'persp-12')
+	plotdiff(expression(testPersp(epand = 0.01)), 'persp-3')
+
 ## test on lim
-	plotdiff(expression(testPersp(xlim = c(-5,5))), 'persp-13')
-  plotdiff(expression(testPersp(ylim = c(-5,5))), 'persp-14')
-  plotdiff(expression(testPersp(zlim = c(-5,5))), 'persp-15')
+	plotdiff(expression(testPersp(xlim = c(-5,5))), 'persp-4')
 
 ## test on label
 plotdiff(expression(
-  testPersp(xlab = 'xx', ylab = 'yy', zlab = 'zz')), 'persp-16')
+  testPersp(xlab = 'xx', ylab = 'yy', zlab = 'zz')), 'persp-5')
 
 ## test on r
-plotdiff(expression(testPersp(r = 5)), 'persp-17')
-plotdiff(expression(testPersp(r = 10)), 'persp-18')
-plotdiff(expression(testPersp(r = 20)), 'persp-19')
-
+plotdiff(expression(testPersp(r = 5)), 'persp-6')
 
 ##test on d
-plotdiff(expression(testPersp(d = 0.5)), 'persp-20')
-plotdiff(expression(testPersp(d = 1)), 'persp-21')
-plotdiff(expression(testPersp(d = 5)), 'persp-22')
+plotdiff(expression(testPersp(d = 0.5)), 'persp-7')
 
 
 ## test on scale
-plotdiff(expression(testPersp(scale = FALSE)), 'persp-23')
-plotdiff(expression(testPersp(scale = TRUE)), 'persp-24')
+plotdiff(expression(testPersp(scale = FALSE)), 'persp-8')
 
 ## test on expand
-plotdiff(expression(testPersp(expand = 0.5)), 'persp-25')
-plotdiff(expression(testPersp(expand = 1)), 'persp-26')
-plotdiff(expression(testPersp(expand = 2)), 'persp-27')
+plotdiff(expression(testPersp(expand = 0.5)), 'persp-9')
 
-## test on col
-plotdiff(expression(testPersp(col = 1)), 'persp-28')
-plotdiff(expression(testPersp(col = 'blue')), 'persp-29')
-plotdiff(expression(testPersp(col = 1:5)), 'persp-30')
-plotdiff(expression(testPersp(col = 1:10)), 'persp-31')
-plotdiff(expression(testPersp(col = 1:100)), 'persp-32')
+
+## test on multi-col
+plotdiff(expression(testPersp(col = 1:5)), 'persp-10')
+
 
 ## test on border
-plotdiff(expression(testPersp(border = 1)), 'persp-33')
-plotdiff(expression(testPersp(border = 'brown')), 'persp-34')
+plotdiff(expression(testPersp(border = 'brown')), 'persp-11')
 # only one color for border
-plotdiff(expression(testPersp(border = 5:6)), 'persp-35')
+plotdiff(expression(testPersp(border = 5:6)), 'persp-12')
 
 ## test on axes
-plotdiff(expression(testPersp(axes = TRUE)), 'persp-36')
-plotdiff(expression(testPersp(axes = FALSE)), 'persp-37')
+plotdiff(expression(testPersp(axes = TRUE)), 'persp-13')
+plotdiff(expression(testPersp(axes = FALSE)), 'persp-14')
 
 ## if box = False then not drawing any axes even axes = TRUE
-plotdiff(expression(testPersp(box = FALSE, axes = TRUE)), 'persp-38')
-plotdiff(expression(testPersp(box = FALSE, axes = FALSE)), 'persp-39')
+plotdiff(expression(testPersp(box = FALSE, axes = TRUE)), 'persp-15')
+plotdiff(expression(testPersp(box = FALSE, axes = FALSE)), 'persp-16')
 
 ## test on ticktype
 plotdiff(expression(
-  testPersp(ticktype = 'detail', axes = TRUE, box = TRUE)), 'persp-40')
+  testPersp(ticktype = 'detail', axes = TRUE, box = TRUE)), 'persp-17')
 
 ## test on lty
-plotdiff(expression(testPersp(lty = 'dotted')), 'persp-41')
-plotdiff(expression(testPersp(lty = '1331')), 'persp-42')
+plotdiff(expression(testPersp(lty = 'dotted')), 'persp-18')
+plotdiff(expression(testPersp(lty = '1331')), 'persp-19')
     
     
 ## test on lwd
-plotdiff(expression(testPersp(lwd = 2)), 'persp-43')
-plotdiff(expression(testPersp(lwd = 3)), 'persp-44')
+plotdiff(expression(testPersp(lwd = 2)), 'persp-20')
+plotdiff(expression(testPersp(lwd = 3)), 'persp-21')
 
 ## other bugs:
     ## 1. pty = 's'
@@ -163,18 +143,10 @@ plotdiff(expression(testPersp(lwd = 3)), 'persp-44')
     ## single colors
 plotdiff(expression(testPersp(col = 'orange', border = 'NA', 
                               shade =0.2, box = FALSE, 
-                              scale = TRUE)), 'persp-45')
+                              scale = TRUE)), 'persp-22')
 plotdiff(expression(testPersp(col = 1:10, border = 'NA', 
                               shade =0.2, box = FALSE, 
-                              scale = TRUE)), 'persp-46')
-plotdiff(expression(testPersp(col = 1:10, border = 'NA', 
-                              shade =0.2, box = FALSE, 
-                              scale = FALSE)), 'persp-47')
-
-## col.lab/cex.lab/col.axis
-plotdiff(expression(testPersp(col.lab = 'red', cex.lab = 1.5, 
-                              col.axis = 'green', ticktype = 'detail')), 
-         'persp-48')
+                              scale = TRUE)), 'persp-23')
 
 #name.graphics = paste('persp-', 1:48, '-graphics.png', sep = '')
 #name.grid = paste('persp-', 1:48, '-grid.png', sep= '')
