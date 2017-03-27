021e13e5bce2f747ff9884535e382ac7e74bdaae
jason <374567155@qq.com>
2017-03-27 17:12:18 +1300

    done

--- R/filled.r
+++ R/filled.r
@@ -103,7 +103,7 @@ lFindCutPoints = function( low,  high,
     if (z1 > z2 ) {
         if (z2 > high || z1 < low){
             return(out = list(x = x, y = y, z = z, npt = npt))
-            }
+        }
 
         if (z1 < high) {
             x[npt + 1] = x1
@@ -122,6 +122,7 @@ lFindCutPoints = function( low,  high,
             z[npt + 1] = z1 + c * (z2 - z1)
             npt = npt + 1
         }
+        
         if (z2 == -Inf) {
             x[npt + 1] = x1
             y[npt + 1] = y1
@@ -134,41 +135,43 @@ lFindCutPoints = function( low,  high,
             z[npt + 1] = z2 - c * (z2 - z1)
             npt = npt + 1
         }
+        
     } else if (z1 < z2) {
         if (z2 < low || z1 > high) {
                 return(out = list(x = x, y = y, z = z, npt = npt))
-                }
-            if (z1 > low) {
-                x[npt + 1] = x1
-                y[npt + 1] = y1
-                z[npt + 1] = z1
-                npt = npt + 1
-            } else if (z1 == -Inf) {
-                x[npt + 1] = x2
-                y[npt + 1] = y1
-                z[npt + 1] = z2
-                npt = npt + 1
-            } else { 
-                c = (z1 - low) / (z1 - z2)
-                x[npt + 1] = x1 + c * (x2 - x1)
-                y[npt + 1] = y1
-                    z[npt + 1] = z1 + c * (z2 - z1)
-                npt = npt + 1
-            }
-            
-            if (z2 < high) {
-            } else if (z2 == Inf) {
-                x[npt + 1] = x1
-                y[npt + 1] = y1
-                z[npt + 1] = z1
-                npt = npt + 1
-            } else {
-                c = (z2 - high) / (z2 - z1)
-                x[npt + 1] = x2 - c * (x2 - x1)
-                y[npt + 1] = y1
-                z[npt + 1] = z2 - c * (z2 - z1)
-                npt = npt + 1
-            }
+        }
+        
+        if (z1 > low) {
+            x[npt + 1] = x1
+            y[npt + 1] = y1
+            z[npt + 1] = z1
+            npt = npt + 1
+        } else if (z1 == -Inf) {
+            x[npt + 1] = x2
+            y[npt + 1] = y1
+            z[npt + 1] = z2
+            npt = npt + 1
+        } else { 
+            c = (z1 - low) / (z1 - z2)
+            x[npt + 1] = x1 + c * (x2 - x1)
+            y[npt + 1] = y1
+                z[npt + 1] = z1 + c * (z2 - z1)
+            npt = npt + 1
+        }
+        
+        if (z2 < high) {
+        } else if (z2 == Inf) {
+            x[npt + 1] = x1
+            y[npt + 1] = y1
+            z[npt + 1] = z1
+            npt = npt + 1
+        } else {
+            c = (z2 - high) / (z2 - z1)
+            x[npt + 1] = x2 - c * (x2 - x1)
+            y[npt + 1] = y1
+            z[npt + 1] = z2 - c * (z2 - z1)
+            npt = npt + 1
+        }
     } else {
         if(low <= z1 && z1 <= high) {
             x[npt + 1] = x1
