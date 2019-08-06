aba0de1e00164437793e757111548121b8e01ddc
Paul Murrell <paul@stat.auckland.ac.nz>
2019-08-07 10:28:27 +1200

    protect against NA text

--- R/text.R
+++ R/text.R
@@ -46,6 +46,8 @@ C_text <- function(x) {
         font <- vfont[2]
         family <- mapVfont(vfont[1])
     }
+    ## Protect against NA labels (until fix in 'grid' is released)
+    labels[is.na(labels)] <- ""
     grid.text(labels, xx, yy, default.units="native",
               hjust=adjx, vjust=adjy, rot=par$srt,
               gp=gpar(cex=cex, col=col, fontface=font, fontfamily=family,
