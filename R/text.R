ddd742583bc15f57bcc514eea55e82de9c83aaec
Paul Murrell <paul@stat.auckland.ac.nz>
2019-10-08 10:45:06 +1300

    add protection for C_text() when 'label' is NULL (github issue #15)

--- R/text.R
+++ R/text.R
@@ -16,43 +16,46 @@ C_text <- function(x) {
     adjy <- just[2]
     pos <- x[[5]]
     offset <- unit(x[[6]]*par$cin[2]*par$cex, "in")
-    if (!is.null(pos)) {
-        n <- length(labels)
-        pos <- rep(pos, length.out=n)
-        adjx <- rep(0.5, length.out=n)
-        # 0.3333 = yCharOffset
-        adjy <- rep(0.3333, length.out=n)
-        xx <- rep(xx, length.out=n)
-        yy <- rep(yy, length.out=n)
-        xx[pos == 2] <- xx - convertWidth(offset, "native", valueOnly=TRUE)
-        xx[pos == 4] <- xx + convertWidth(offset, "native", valueOnly=TRUE)
-        yy[pos == 1] <- yy - convertHeight(offset, "native", valueOnly=TRUE)
-        yy[pos == 3] <- yy + convertHeight(offset, "native", valueOnly=TRUE)
-        adjx[pos == 2] <- 1
-        adjx[pos == 4] <- 0   
-        adjy[pos == 1] <- 1 - (0.5 - 0.3333) 
-        adjy[pos == 3] <- 0
+    ## NULL label generates no grob
+    if (!is.null(labels)) {
+        if (!is.null(pos)) {
+            n <- length(labels)
+            pos <- rep(pos, length.out=n)
+            adjx <- rep(0.5, length.out=n)
+                                        # 0.3333 = yCharOffset
+            adjy <- rep(0.3333, length.out=n)
+            xx <- rep(xx, length.out=n)
+            yy <- rep(yy, length.out=n)
+            xx[pos == 2] <- xx - convertWidth(offset, "native", valueOnly=TRUE)
+            xx[pos == 4] <- xx + convertWidth(offset, "native", valueOnly=TRUE)
+            yy[pos == 1] <- yy - convertHeight(offset, "native", valueOnly=TRUE)
+            yy[pos == 3] <- yy + convertHeight(offset, "native", valueOnly=TRUE)
+            adjx[pos == 2] <- 1
+            adjx[pos == 4] <- 0   
+            adjy[pos == 1] <- 1 - (0.5 - 0.3333) 
+            adjy[pos == 3] <- 0
+        }
+        vfont <- x[[7]]
+        cex <- FixupCex(x[[8]]*par$cex, 1)
+        cex <- ifelse(is.na(cex), par$cex, cex)
+        col <- FixupCol(x[[9]], NA, par$bg)
+        col <- ifelse(is.na(col), par$col, col)
+        font <- FixupFont(x[[10]], NA)
+        font <- ifelse(is.na(font), par$font, font)
+        family <- par$family
+        if (!is.null(vfont) && !is.language(labels)) {
+                                        # Override 'font' and 'family'
+            font <- vfont[2]
+            family <- mapVfont(vfont[1])
+        }
+        ## Protect against NA labels
+        labels[is.na(labels)] <- ""
+        grid.text(labels, xx, yy, default.units="native",
+                  hjust=adjx, vjust=adjy, rot=par$srt,
+                  gp=gpar(cex=cex, col=col, fontface=font, fontfamily=family,
+                          lineheight=par$lheight),
+                  name=grobname("text"))
     }
-    vfont <- x[[7]]
-    cex <- FixupCex(x[[8]]*par$cex, 1)
-    cex <- ifelse(is.na(cex), par$cex, cex)
-    col <- FixupCol(x[[9]], NA, par$bg)
-    col <- ifelse(is.na(col), par$col, col)
-    font <- FixupFont(x[[10]], NA)
-    font <- ifelse(is.na(font), par$font, font)
-    family <- par$family
-    if (!is.null(vfont) && !is.language(labels)) {
-        # Override 'font' and 'family'
-        font <- vfont[2]
-        family <- mapVfont(vfont[1])
-    }
-    ## Protect against NA labels 
-    labels[is.na(labels)] <- ""
-    grid.text(labels, xx, yy, default.units="native",
-              hjust=adjx, vjust=adjy, rot=par$srt,
-              gp=gpar(cex=cex, col=col, fontface=font, fontfamily=family,
-                  lineheight=par$lheight),
-              name=grobname("text"))
     upViewport(depth)
 }
 
